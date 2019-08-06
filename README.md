e44d2e709ccf442a318facc477b9915b7df040f3
shirleytwl <shirtanwl@gmail.com>
2019-08-06 10:25:50 +0800

    Added more responsiveness and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # weview
+A review site for YouTube Channels.
+
+## Technologies used
+
+-   Express
+-   Node.js
+-   React view
+-   Materialize.css
+-   Materialize-stepper
+-   YouTube API
+-   Cloudinary
+
+
+## Future features to add in
+
+- [ ] Update Username
+- [ ] Users can add in other channel recommendations for a specific channels
+
+## Check out the website here: 
+https://yt-weview.herokuapp.com/
+
--- views/components/form-channel.jsx
+++ views/components/form-channel.jsx
@@ -23,20 +23,20 @@ class ChannelForm extends React.Component {
 						<div className="step-title waves-effect waves-dark">Write Review</div>
 						<div className="step-content step-2">
 							<div className="row">
-								<div className="col s2">
+								<div className="col s6 m2">
 									<img className="responsive-img channel-thumbnail"/>
 								</div>
-								<div className="col s10">
+								<div className="col s12 m10">
 									<h5 className="channel-name"></h5>
 									<div className="channel-categories"></div>
 								</div>
 							</div>
 							<div className="row valign-wrapper">
-								<div className="col s2">
+								<div className="col s4 m2">
 									<p className="rating-score" htmlFor="channel_rating">Rating <span className="teal-text">0/5</span></p>
 
 								</div>
-								<div className="col s10">
+								<div className="col s8 m10">
 									<p className="range-field">
 										<input name="channel_rating" type="range" id="channel_rating" min="0" max="5" value="0"/>
 									</p>
@@ -58,20 +58,20 @@ class ChannelForm extends React.Component {
 						<div className="step-title waves-effect waves-dark">Confirm Review</div>
 						<div className="step-content step-3">
 							<div className="row">
-								<div className="col s2">
+								<div className="col s6 m2">
 									<img className="responsive-img channel-thumbnail"/>
 								</div>
-								<div className="col s10">
+								<div className="col s12 m10">
 									<h5 className="channel-name"></h5>
 									<div className="channel-categories"></div>
 								</div>
 							</div>
 							<div className="row valign-wrapper">
-								<div className="col s2">
+								<div className="col s4 m2">
 									<p className="overview-score" htmlFor="channel_rating">Rating <span className="teal-text">0/5</span></p>
 
 								</div>
-								<div className="col s10">
+								<div className="col s8 m10">
 									<p className="range-field">
 										<input name="channel_rating" type="range" id="overview_rating" min="0" max="5" disabled/>
 									</p>
